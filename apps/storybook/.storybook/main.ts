e19589ab8927b0078355e2f3fcdeb05a4e8116a1
mgasic <48404494+mgasic@users.noreply.github.com>
2025-11-01 17:03:50 +0100

    refactor: align app deps with central catalog

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -11,14 +11,13 @@ function getAbsolutePath(value: string): any {
 
 const config: StorybookConfig = {
   stories: [
-    '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
-    '../../../packages/core/src/components/**/*.stories.@(js|jsx|mjs|ts|tsx)'
+    '../stories/**/*.mdx',
+    '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'
   ],
   addons: [
     getAbsolutePath('@storybook/addon-essentials'),
     getAbsolutePath('@storybook/addon-interactions'),
     getAbsolutePath('@storybook/addon-a11y'),
-    'storybook-design-token',
     getAbsolutePath('@storybook/addon-docs')
   ],
   framework: {
