a586e6555f9bb84a10ef48dac2dceba3de0e372f
Sasonal Dekant <sasonal.dekant@gmail.com>
2025-11-09 01:24:30 +0100

    fix: cleanup test setup for jest-axe matcher, use only toHaveNoViolations matcher and remove extra matcher signatures

--- packages/core/test/setup.ts
+++ packages/core/test/setup.ts
@@ -2,10 +2,8 @@ import '@testing-library/jest-dom';
 import { expect } from 'vitest';
 import { toHaveNoViolations } from 'jest-axe';
 
-// Extend Vitest's expect with jest-axe matcher
 expect.extend({ toHaveNoViolations });
 
-// Type declarations
 declare module 'vitest' {
   interface Assertion<T = any> {
     toHaveNoViolations(): T;
